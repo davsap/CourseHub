823f0d7076784f82276088ea2b3af4e773b19edb
davsap <david.parto@hotmail.com>
2018-07-29 00:23:52 +0200

    Un petit test sur git , rien important

--- backend/coursehubcode/src/main/java/fr/coursehub/model/Formateur.java
+++ backend/coursehubcode/src/main/java/fr/coursehub/model/Formateur.java
@@ -6,4 +6,11 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Formateur")
 public class Formateur extends Utilisatuer{
+
+    public Formateur(){}
+
+    public Formateur(String prenom, String nom, String email, String phoneNumber) {
+        super(prenom, nom, email, phoneNumber);
+    }
+
 }
