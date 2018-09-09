1281d81d32707483ec9cba928eb4360b45c60b6c
davsap <david.parto@hotmail.com>
2018-09-09 21:54:24 +0200

    CoursForm: Upload video ajouté, coursdetail mis a jour, coursService ,is a jours

--- src/app/content/cours/coursform/coursform.component.ts
+++ src/app/content/cours/coursform/coursform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import {Cours} from './../../../model/cours';
+import { CoursService } from './../../../services/cours.service';
 
 @Component({
   selector: 'app-coursform',
@@ -7,11 +9,28 @@ import { FormControl} from '@angular/forms';
   styleUrls: ['./coursform.component.css']
 })
 export class CoursFormComponent implements OnInit {
+   coursForm = this.fb.group({
+    titre: [null , [Validators.required, Validators.minLength(1)]],
+    description: [null , [Validators.required, Validators.minLength(1)]],
+      contenuCours: [null , [Validators.required]],
+      coursvideo: [null , [Validators.required]]
+    });
+cours: Cours;
 
-  coursForm = new FormControl('');
-  constructor() { }
+  constructor(private fb: FormBuilder, private service: CoursService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {}
+
+  onSubmit() {
+      // TODO: Use EventEmitter with form value
+      console.log('Formulaire soumis : ' + this.coursForm.value);
+      this.cours = new Cours(1,
+      this.coursForm.get('titre').value,
+      this.coursForm.get('description').value,
+      this.coursForm.get('contenuCours').value,
+      this.coursForm.get('coursvideo').value);
+      console.log('Formulaire soumis : ' + this.cours.contenu);
+      this.service.addCours(this.cours);
   }
 
 }
