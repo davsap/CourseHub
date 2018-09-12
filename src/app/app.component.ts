9dd9374517c2489b657dc17df99e9f8996d83bac
JulieB47 <julie.bergagnini@gmail.com>
2018-09-12 16:20:51 +0200

    App component pour david

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 
   })
-
+// comment
 export class AppComponent {
     private toggleSideBar() {
       document.getElementById('sidebar').classList.toggle('expanded');
