5871dcbcace6121f84259b86d0022b458037b714
magdalena <m.chmielowska@hotmail.com>
2018-09-14 18:15:40 +0200

    Creation Cards

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -9,12 +9,13 @@ import { LoginComponent } from './login/login.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FormationModule } from './formation/formation.module';
+import { CardsComponent } from '../cards/cards.component';
 
 @NgModule({
   imports: [
  CommonModule, RouterModule, SliderModule,  FormationModule, ReactiveFormsModule, FormsModule ],
   declarations: [ContentComponent, HomeComponent, FormationComponent, LoginComponent,
-                  InscriptionComponent],
-  exports: [ContentComponent, HomeComponent, FormationComponent, LoginComponent, InscriptionComponent  ]
+                  InscriptionComponent, CardsComponent],
+  exports: [ContentComponent, HomeComponent, FormationComponent, LoginComponent, InscriptionComponent, CardsComponent  ]
       })
 export class ContentModule { }
