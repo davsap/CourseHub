be96b1abf102c8bd174e559d0b7fd6f99b9db6d7
JulieB47 <julie.bergagnini@gmail.com>
2018-09-16 16:54:20 +0200

    MaJ bouton retour dans le détail d'un cours

--- src/app/content/formation/sujet/cours/cours-detail/cours-detail.component.ts
+++ src/app/content/formation/sujet/cours/cours-detail/cours-detail.component.ts
@@ -11,14 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 export class CoursDetailComponent implements OnInit {
   cours: Cours;
   coursId: number;
+  id: number;
 
   constructor(private service: CoursService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
+    this.id = +this.route.snapshot.paramMap.get('id');
     const ids = +this.route.snapshot.paramMap.get('ids');
     const idc = +this.route.snapshot.paramMap.get('idc');
-    this.service.getCoursV2(id, ids, idc).subscribe((value: Cours) => this.cours = value);
+    this.service.getCoursV2(this.id, ids, idc).subscribe((value: Cours) => this.cours = value);
   }
 
 }
